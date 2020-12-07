ca384a98254f7908bb0ef4a828f7b2b6b394e280
J Rhee <jayrhee0514@gmail.com>
2020-12-07 17:15:06 -0500

    added jawsdb to my conneciton.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burger", function(req, res) {
   burger.create([
-    "name", "sleepy"
+    "name", "devoured"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -33,22 +33,9 @@ router.put("/api/burger/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burger/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
